084a27efa5c8f4835ebfc3e67879c6eeed2a708d
akhil <akhilvijayanponmudy@gmail.com>
2021-03-09 08:47:55 +0530

    09-03-2021 javascript

---
+++ javascript/fundamentals/script.js
@@ -0,0 +1,31 @@
+console.log("Hello World")
+
+
+alert("Welcome")
+
+
+//variables
+//var,let,const
+//var global declaration
+//let block level
+//cont not changable
+
+
+
+var cname="Luminar Technolab"
+console.log("Company name is"+cname);
+
+
+var num=10;  //int
+var avg=17.5;  //float
+var name="luminar"  //str
+var isActive=true;   //boolean
+
+
+console.log(typeof(num))
+
+
+
+
+
+
